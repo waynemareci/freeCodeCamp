dd42eadafca99eddc56dbff5be69f96e41dce659
waynemareci <wayne@mareci.com>
2025-03-22 23:52:12 +0000

    finished

--- countdown.sh
+++ countdown.sh
@@ -2,22 +2,26 @@
 
 # Program that counts down to zero from a given argument
 
+echo -e "\n~~ Countdown Timer ~~\n"
+
 if [[ $1 -gt 0 ]]
 then
-  echo -e "\n~~ Countdown Timer ~~\n"
- : ' for (( i = $1; i >= 0; i-- )) 
-  do 
-    echo $i 
+  : '
+  for (( i = $1; i >= 0; i-- ))
+  do
+    echo $i
     sleep 1
-  done '
+  done
+  '
+
   I=$1
+
   while [[ $I -ge 0 ]]
   do
-
-    echo $I;
+    echo $I
     (( I-- ))
     sleep 1
   done
 else
-  echo "Include a positive integer as the first argument."
+  echo Include a positive integer as the first argument.
 fi
